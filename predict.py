3938298e308ed005403e69d0d72cf9abc4a770e1
Sri Rangan <srirangan0603@gmail.com>
2026-06-23 22:25:41 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -2,6 +2,8 @@ import joblib
 import numpy as np
 import pandas as pd
 import sys
+
+
 # ─── Load Model & Files Safely ─────────────────────────────
 try:
     model        = joblib.load('best_model.pkl')
