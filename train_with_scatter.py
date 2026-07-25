3331a918c3c820fb28b9a5ad1239fa14956322d9
Sri Rangan <srirangan0603@gmail.com>
2026-07-25 19:56:15 +0530

    Update train_with_scatter.py

--- train_with_scatter.py
+++ train_with_scatter.py
@@ -14,9 +14,6 @@ warnings.filterwarnings('ignore')
 
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import (accuracy_score, f1_score, roc_auc_score, confusion_matrix)
-
-
-
 from sklearn.metrics import (accuracy_score, f1_score, roc_auc_score,confusion_matrix)
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
